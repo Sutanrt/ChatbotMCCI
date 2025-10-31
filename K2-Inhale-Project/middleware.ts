805c3ecd77d12d5fe1103d71b04a42488bbf5766
unknown <sutankii@gmail.com>
2025-10-31 13:14:40 +0700

    test

--- K2-Inhale-Project/middleware.ts
+++ K2-Inhale-Project/middleware.ts
@@ -1,35 +1,35 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-import { guestRegex, isDevelopmentEnvironment } from "./lib/constants.edge";
+// import { guestRegex, isDevelopmentEnvironment } from "./lib/constants.edge";
 
 export async function middleware(request: NextRequest) {
-  // const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
-  // /*
-  //  * Playwright starts the dev server and requires a 200 status to
-  //  * begin the tests, so this ensures that the tests can start
-  //  */
-  // if (pathname.startsWith("/ping")) {
-  //   return new Response("pong", { status: 200 });
-  // }
+  /*
+   * Playwright starts the dev server and requires a 200 status to
+   * begin the tests, so this ensures that the tests can start
+   */
+  if (pathname.startsWith("/ping")) {
+    return new Response("pong", { status: 200 });
+  }
 
-  // if (pathname.startsWith("/api/auth")) {
-  //   return NextResponse.next();
-  // }
+  if (pathname.startsWith("/api/auth")) {
+    return NextResponse.next();
+  }
 
-  // const token = await getToken({
-  //   req: request,
-  //   secret: process.env.AUTH_SECRET,
-  //   secureCookie: !isDevelopmentEnvironment,
-  // });
+  const token = await getToken({
+    req: request,
+    secret: process.env.AUTH_SECRET,
+    // secureCookie: !isDevelopmentEnvironment,
+  });
 
-  // if (!token) {
-  //   const redirectUrl = encodeURIComponent(request.url);
+  if (!token) {
+    const redirectUrl = encodeURIComponent(request.url);
 
-  //   return NextResponse.redirect(
-  //     new URL(`/api/auth/guest?redirectUrl=${redirectUrl}`, request.url)
-  //   );
-  // }
+    return NextResponse.redirect(
+      new URL(`/api/auth/guest?redirectUrl=${redirectUrl}`, request.url)
+    );
+  }
 
   // const isGuest = guestRegex.test(token?.email ?? "");
 
@@ -37,7 +37,7 @@ export async function middleware(request: NextRequest) {
   //   return NextResponse.redirect(new URL("/", request.url));
   // }
 
-  // return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
@@ -54,8 +54,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-
     "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-  
   ],
 };
